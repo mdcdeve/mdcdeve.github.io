06518a2bd5e62e5f2d0c2757a4f256f46804127b
mdcdeve <124759000+mdcdeve@users.noreply.github.com>
2023-02-08 00:34:27 -0500

    Update java.js

--- java.js
+++ java.js
@@ -1,36 +1,3 @@
-const submitButton = document.getElementById("submit");
-const output = document.getElementById("bitlyshort");
-
-submitButton.addEventListener("click", async (event) => {
-  event.preventDefault();
-
-  const url = document.getElementById("output").value;
-
-  const response = await fetch(`https://api-ssl.bitly.com/v4/shorten`, {
-    method: "POST",
-    headers: {
-      Authorization: "Bearer bfbe3808572eff5694908d03e9f05cfc8fb1951b",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      long_url: url
-    })
-  });
-
-  const data = await response.json();
-
-  output.innerHTML = `Short Boy ~ <a href="${data.link}">${data.link}</a>`;
-});
-
-function copyToClipboard(str) {
-  var el = document.createElement("textarea");
-  el.value = str;
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand("copy");
-  document.body.removeChild(el);
-}
-
 function splitString() {
   var input = document.getElementById("input").value;
   if (input.includes("hottopic.com")) {
