9b1f50d3676778c2bd5f6f32d608cd731af8905d
mdcdeve <124759000+mdcdeve@users.noreply.github.com>
2023-02-15 23:57:49 -0500

    Update java.js

--- java.js
+++ java.js
@@ -81,6 +81,40 @@ function splitString() {
   } else {
     part1 = "Nope";
   }
+
+  document.getElementById("output").value = part1;
+
+  copyToClipboard(part1);
+}
+
+function noSplit() {
+  var input = document.getElementById("input").value;
+  var part1 = "";
+  
+  if (input.includes("hottopic.com")) {
+    part1 = "https://www.dpbolvw.net/click-8310556-12641438?url=" + input;
+  } else if (input.includes("boxlunch.com")) {
+    part1 = "https://www.tkqlhce.com/click-8310556-12695057?url=" + input;
+  } else if (input.includes("shopdisney.com")) {
+    part1 = "https://www.tkqlhce.com/click-8310556-13227368?url=" + input;
+  } else if (input.includes("gamestop.com")) {
+    part1 = "https://click.linksynergy.com/deeplink?id=IdYcgGrVl44&mid=24348&murl=" + input;
+  } else if (input.includes("walgreens.com")) {
+    part1 = "https://www.jdoqocy.com/click-8310556-11553095?url=" + input;
+  } else if (input.includes("lego.com")) {
+    part1 = "https://click.linksynergy.com/deeplink?id=IdYcgGrVl44&mid=13923&murl=" + input;
+  } else if (input.includes("booksamillion.com")) {
+    part1 = "https://www.tkqlhce.com/click-8310556-11552245?url=" + input;
+  } else if (input.includes("walmart.com")) {
+    part1 = "https://goto.walmart.com/c/899674/565706/9383?veh=aff&sourceid=imp_000011112222333344&u=" + input;
+  } else if (input.includes("target.com")) {
+    part1 = "https://www.ojrq.net/p/?return=https://goto.target.com/c/373031/81938/2092?u=" + input;
+  } else if (input.includes("bestbuy.com")) {
+    part1 = "https://bestbuy.7tiv.net/c/373031/614286/10014?u=" + input;
+  } else {
+    part1 = "No Split";
+  }
+  
   document.getElementById("output").value = part1;
   copyToClipboard(part1);
 }
