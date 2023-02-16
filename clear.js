760abdafddacd2c93f14ef020fd7c5006b75b3b3
mdcdeve <124759000+mdcdeve@users.noreply.github.com>
2023-02-15 23:58:07 -0500

    Create clear.js

---
+++ clear.js
@@ -0,0 +1,10 @@
+function clearElements() {
+  // Empty input textbox
+  document.getElementById("input").value = "";
+
+  // Empty output textbox
+  document.getElementById("output").value = "";
+
+  // Empty bitlyshort div
+  document.getElementById("bitlyshort").innerHTML = "";
+}
