73bc9ed1d215ab4f5ec6230fdd4fadfccf7dfefe
mdcdeve <124759000+mdcdeve@users.noreply.github.com>
2023-07-17 02:20:59 -0400

    Update nosplit.js

--- nosplit.js
+++ nosplit.js
@@ -9,13 +9,25 @@ function noSplit() {
     "lego.com": "https://click.linksynergy.com/deeplink?id=IdYcgGrVl44&mid=13923&murl=",
     "booksamillion.com": "https://www.tkqlhce.com/click-8310556-11552245?url=",
     "walmart.com": "https://goto.walmart.com/c/899674/565706/9383?veh=aff&sourceid=imp_000011112222333344&u=",
-    "target.com": "https://www.ojrq.net/p/?return=https://goto.target.com/c/373031/81938/2092?u=",
-    "bestbuy.com": "https://bestbuy.7tiv.net/c/373031/614286/10014?u="
+    "target.com": "https://goto.target.com/c/373031/81938/2092?u=",
+    "bestbuy.com": "https://bestbuy.7tiv.net/c/373031/614286/10014?u=",
+    "amazon.com": "?&linkCode=ll1&tag=fupone-20&linkId=7fd95c65fd643211a87550fcef50c597&language=en_US&ref_=as_li_ss_tl"
   };
 
-  const domain = Object.keys(affiliateLinks).find((domain) => input.includes(domain));
-  const part1 = domain ? affiliateLinks[domain] + input : "No Split";
-  
+  const domain = Object.keys(affiliateLinks).find((domain) =>
+    input.includes(domain)
+  );
+
+  let part1;
+  if (domain) {
+    part1 =
+      domain === "amazon.com"
+        ? input + affiliateLinks[domain]
+        : affiliateLinks[domain] + input;
+  } else {
+    part1 = "No Split";
+  }
+
   document.getElementById("output").value = part1;
   copyToClipboard(part1);
 }
