f196c5f886685139366a5f7d24aaf9d64af9aad4
mdcdeve <124759000+mdcdeve@users.noreply.github.com>
2025-02-18 23:11:51 +0000

    Update nosplit.js

--- nosplit.js
+++ nosplit.js
@@ -4,18 +4,19 @@ function noSplit() {
     "hottopic.com": "https://www.tkqlhce.com/click-8310556-12641438?url=",
     "boxlunch.com": "https://www.tkqlhce.com/click-8310556-12695057?url=",
     "disneystore.com": "https://www.anrdoezrs.net/click-8310556-11554175?url=",
-    "gamestop.com": "https://click.linksynergy.com/deeplink?id=IdYcgGrVl44&mid=24348&murl=",
     "walgreens.com": "https://www.anrdoezrs.net/click-8310556-11553095?url=",
-    "lego.com": "https://click.linksynergy.com/deeplink?id=IdYcgGrVl44&mid=13923&murl=",
     "booksamillion.com": "https://www.anrdoezrs.net/click-8310556-11552245?url=",
-    "walmart.com": "https://goto.walmart.com/c/899674/565706/9383?veh=aff&sourceid=imp_000011112222333344&u=",
-    "target.com": "https://goto.target.com/c/373031/81938/2092?u=",
-    "bestbuy.com": "https://bestbuy.7tiv.net/c/373031/614286/10014?u=",
     "funko.com": "https://www.dpbolvw.net/click-8310556-15251747?url=",
-    "amazon.com": "?linkCode=ll1&tag=fupone-20&linkId=7fd95c65fd643211a87550fcef50c597&language=en_US&ref_=as_li_ss_tl",
     "barnesandnoble.com": "https://www.kqzyfj.com/click-8310556-12366831?url=",
     "2ndandcharles.com": "https://www.jdoqocy.com/click-8310556-15494442?url=",
-    "fye.com": "https://www.kqzyfj.com/click-8310556-15558078?url="
+    "fye.com": "https://www.kqzyfj.com/click-8310556-15558078?url=",
+    "amazon.com": "&linkCode=ll2&tag=fupone-20&linkId=b368190afcd29e05f4e85f63a9579031&language=en_US&ref_=as_li_ss_tl",
+    "gamestop.com": "https://click.linksynergy.com/deeplink?id=IdYcgGrVl44&mid=24348&murl=",
+    "lego.com": "https://click.linksynergy.com/deeplink?id=IdYcgGrVl44&mid=13923&murl=",
+    "wwe.com": "https://wwe-shop.sjv.io/c/899674/1371040/16449?u=",
+    "walmart.com": "https://goto.walmart.com/c/899674/565706/9383?veh=aff&sourceid=imp_000011112222333344&u=",
+    "target.com": "https://goto.target.com/c/373031/81938/2092?u=",
+    "bestbuy.com": "https://bestbuy.7tiv.net/c/373031/614286/10014?u="
   };
 
   const domain = Object.keys(affiliateLinks).find((domain) =>
